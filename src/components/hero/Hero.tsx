179843742cc3d1262887082a35e63aa39998b661
Hila Medina <hilam@moveo.co.il>
2022-12-19 10:23:47 +0200

    first commit

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import hila from "./hero-picture1.png";
+
+const Hero = () => {
+	return (
+		<div className="hero">
+			<div className="search-and-h1">
+				<div>
+					<div className="hero-text">
+						<p>hila msavpasv`ksbpv`dbk`</p>
+						<div style={{ border: "1px red solid" }}>
+							<input></input>
+						</div>
+					</div>
+					{/* <input></input> */}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
