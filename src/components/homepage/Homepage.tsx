3ae14c8090c728a2ca457eb0eb06bf2fcee7d7d4
Hila Medina <hilam@moveo.co.il>
2022-12-19 14:35:49 +0200

    signaturedish

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "../hero/Hero";
 import LowerComponentCarruosel from "../lowerComponentCarruosel/LowerComponentCarruosel";
+import SignatureDish from "../signaturedish/SignatureDish";
 import UpperComponentCarrousel from "../upperHomepageCarrousel/UpperComponentCarrousel";
 
 const Homepage = () => {
@@ -9,6 +10,7 @@ const Homepage = () => {
 			<Hero></Hero>
 			<UpperComponentCarrousel></UpperComponentCarrousel>
 			<LowerComponentCarruosel></LowerComponentCarruosel>
+			<SignatureDish></SignatureDish>
 		</div>
 	);
 };
